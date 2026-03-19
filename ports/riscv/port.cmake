8a1f35b20121db764e072114b663e6f45c430fca
sohail103 <sohailraj.satapathy@gmail.com>
2026-03-19 20:24:20 +0530

    port/riscv: implement nn_init and nn_classify using muriscv-nn

--- ports/riscv/port.cmake
+++ ports/riscv/port.cmake
@@ -1,4 +1,6 @@
-include_directories(${PORT_DIR})
+include_directories(
+    ${PORT_DIR}
+    ${PORT_DIR}/muriscv-nn/Include)
 
 file(GLOB PORT_SOURCES
     ${PORT_DIR}/*.c
@@ -7,6 +9,24 @@ file(GLOB PORT_SOURCES
     ${PORT_DIR}/packed_simd/*.c
 )
 
+file(GLOB MURISCV_NN_SOURCES
+    ${PORT_DIR}/muriscv-nn/Source/ConvolutionFunctions/*.c
+    ${PORT_DIR}/muriscv-nn/Source/DepthwiseConvolutionFunctions/*.c
+    ${PORT_DIR}/muriscv-nn/Source/FullyConnectedFunctions/*.c
+    ${PORT_DIR}/muriscv-nn/Source/PoolingFunctions/*.c
+    ${PORT_DIR}/muriscv-nn/Source/SoftmaxFunctions/*.c
+    ${PORT_DIR}/muriscv-nn/Source/NNSupportFunctions/*.c
+)
+
+# remove unwanted files
+list(FILTER MURISCV_NN_SOURCES EXCLUDE REGEX ".*lstm.*")
+list(FILTER MURISCV_NN_SOURCES EXCLUDE REGEX ".*svdf.*")
+list(FILTER MURISCV_NN_SOURCES EXCLUDE REGEX ".*transpose.*")
+list(FILTER MURISCV_NN_SOURCES EXCLUDE REGEX ".*_1_x_n_s4.*")
+
 add_definitions(-DUSE_SMALLFT)
 
-set(PORT_SOURCE ${PORT_SOURCES})
+set(PORT_SOURCE 
+    ${PORT_SOURCES}
+    ${MURISCV_NN_SOURCES}
+)
