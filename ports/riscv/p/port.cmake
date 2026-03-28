bdbf4cf39789dddc62e60f4f31649f10da4c0748
JRobinNTA <johnrobin044@gmail.com>
2026-03-28 22:32:56 +0530

    ports/riscv/:synced port.cmake to upstream

--- ports/riscv/p/port.cmake
+++ ports/riscv/p/port.cmake
@@ -6,8 +6,25 @@ include_directories(
 file(GLOB PORT_SOURCES
     ${PORT_DIR}/th_api.c
     ${PORT_DIR}/src/*.c
-    ${PORT_DIR}/src/p_riscv_cfft/*.c
-    ${PORT_DIR}/src/p_riscv_conv/*.c
+
+
+  # NOTE: these sources are temporary added from scalar port as rvv port for
+  # them is not yet implemented.
+  ${PORT_DIR}/../src/riscv_cmplx_mult_cmplx_f32.c
+  ${PORT_DIR}/../src/riscv_cmplx_conj_f32.c
+  ${PORT_DIR}/../src/riscv_cmplx_dot_prod_f32.c
+  ${PORT_DIR}/../src/riscv_dot_prod_f32.c
+  ${PORT_DIR}/../src/riscv_cmplx_mag_f32.c
+  ${PORT_DIR}/../src/riscv_vlog_f32.c
+  ${PORT_DIR}/../src/riscv_mat_vec_mult_f32.c
+  ${PORT_DIR}/../src/riscv_nn_init.c
+  ${PORT_DIR}/../src/riscv_nn_classify.c
+  ${PORT_DIR}/../src/riscv_add_f32.c
+  ${PORT_DIR}/../src/riscv_subtract_f32.c
+  ${PORT_DIR}/../src/riscv_offset_f32.c
+  ${PORT_DIR}/../src/riscv_multiply_f32.c
+  ${PORT_DIR}/../src/riscv_f32_to_int16.c
+  ${PORT_DIR}/../src/riscv_int16_to_f32.c
 )
 
 # TODO: Add our FFT functions as an FFT backend in fftwrap.c
