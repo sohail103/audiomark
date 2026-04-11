08788a61941ab64c073fe4fbfe6746771cc85cc6
sohail103 <sohailraj.satapathy@gmail.com>
2026-04-11 13:05:06 +0530

    port/riscv/v: drop riscv_ prefix and cleanup port structure

--- ports/riscv/v/port.cmake
+++ ports/riscv/v/port.cmake
@@ -1,32 +1,40 @@
 include_directories(
-  ${PORT_DIR}
-  ${PORT_DIR}/inc
-)
-
-file(GLOB RISCV_V_SOURCES
-  ${PORT_DIR}/th_api.c
-  ${PORT_DIR}/src/*.c
-
-  # NOTE: these sources are temporary added from scalar port as rvv port for
-  # them is not yet implemented.
-  ${PORT_DIR}/../src/riscv_cfft_init_f32.c
-  ${PORT_DIR}/../src/riscv_cfft_f32.c
-  ${PORT_DIR}/../src/riscv_rfft_init_f32.c
-  ${PORT_DIR}/../src/riscv_rfft_f32.c
-  ${PORT_DIR}/../src/riscv_absmax_f32.c
-  ${PORT_DIR}/../src/riscv_cmplx_mult_cmplx_f32.c
-  ${PORT_DIR}/../src/riscv_cmplx_conj_f32.c
-  ${PORT_DIR}/../src/riscv_cmplx_dot_prod_f32.c
-  ${PORT_DIR}/../src/riscv_dot_prod_f32.c
-  ${PORT_DIR}/../src/riscv_cmplx_mag_f32.c
-  ${PORT_DIR}/../src/riscv_vlog_f32.c
-  ${PORT_DIR}/../src/riscv_mat_vec_mult_f32.c
-  ${PORT_DIR}/../src/riscv_nn_init.c
-  ${PORT_DIR}/../src/riscv_nn_classify.c
+    ${PORT_DIR}/..
 )
 
 add_definitions(-DUSE_SMALLFT)
 
 set(PORT_SOURCE 
-  ${RISCV_V_SOURCES}
+
+    ${PORT_DIR}/../th_api.c
+
+    ${PORT_DIR}/../src/nn/avgpool_s8.c
+    ${PORT_DIR}/../src/nn/convolve_s8.c
+    ${PORT_DIR}/../src/nn/depthwise_conv_s8.c
+    ${PORT_DIR}/../src/nn/fully_connected_s8.c
+    ${PORT_DIR}/../src/nn/mat_mult_kernel_s8_s16.c
+    ${PORT_DIR}/../src/nn/q7_to_q15_with_offset.c
+    ${PORT_DIR}/../src/nn/softmax_s8.c
+    ${PORT_DIR}/../src/nn/vec_mat_mult_t_s8.c
+    
+    ${PORT_DIR}/../src/absmax_f32.c
+    ${PORT_DIR}/src/add_f32.c
+    ${PORT_DIR}/../src/cfft_f32.c
+    ${PORT_DIR}/../src/cfft_init_f32.c
+    ${PORT_DIR}/../src/cmplx_conj_f32.c
+    ${PORT_DIR}/../src/cmplx_dot_prod_f32.c
+    ${PORT_DIR}/../src/cmplx_mag_f32.c
+    ${PORT_DIR}/../src/cmplx_mult_cmplx_f32.c
+    ${PORT_DIR}/../src/dot_prod_f32.c
+    ${PORT_DIR}/src/f32_to_int16.c
+    ${PORT_DIR}/src/int16_to_f32.c
+    ${PORT_DIR}/../src/mat_vec_mult_f32.c
+    ${PORT_DIR}/src/multiply_f32.c
+    ${PORT_DIR}/../src/nn_classify.c
+    ${PORT_DIR}/../src/nn_init.c
+    ${PORT_DIR}/src/offset_f32.c
+    ${PORT_DIR}/../src/rfft_f32.c
+    ${PORT_DIR}/../src/rfft_init_f32.c
+    ${PORT_DIR}/src/subtract_f32.c
+    ${PORT_DIR}/../src/vlog_f32.c
 )
