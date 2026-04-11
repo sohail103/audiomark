fcb8f94524f464a72c0c37a2984a24e14b3dbff4
sohail103 <sohailraj.satapathy@gmail.com>
2026-04-11 12:13:47 +0530

    port/riscv: drop riscv_ prefix in file names

--- ports/riscv/port.cmake
+++ ports/riscv/port.cmake
@@ -20,24 +20,24 @@ set(PORT_SOURCE
     ${PORT_DIR}/src/nn/softmax_s8.c
     ${PORT_DIR}/src/nn/vec_mat_mult_t_s8.c
 
-    ${PORT_DIR}/src/riscv_absmax_f32.c
-    ${PORT_DIR}/src/riscv_add_f32.c
-    ${PORT_DIR}/src/riscv_cfft_f32.c
-    ${PORT_DIR}/src/riscv_cfft_init_f32.c
-    ${PORT_DIR}/src/riscv_cmplx_conj_f32.c
-    ${PORT_DIR}/src/riscv_cmplx_dot_prod_f32.c
-    ${PORT_DIR}/src/riscv_cmplx_mag_f32.c
-    ${PORT_DIR}/src/riscv_cmplx_mult_cmplx_f32.c
-    ${PORT_DIR}/src/riscv_dot_prod_f32.c
-    ${PORT_DIR}/src/riscv_f32_to_int16.c
-    ${PORT_DIR}/src/riscv_int16_to_f32.c
-    ${PORT_DIR}/src/riscv_mat_vec_mult_f32.c
-    ${PORT_DIR}/src/riscv_multiply_f32.c
-    ${PORT_DIR}/src/riscv_nn_classify.c
-    ${PORT_DIR}/src/riscv_nn_init.c
-    ${PORT_DIR}/src/riscv_offset_f32.c
-    ${PORT_DIR}/src/riscv_rfft_f32.c
-    ${PORT_DIR}/src/riscv_rfft_init_f32.c
-    ${PORT_DIR}/src/riscv_subtract_f32.c
-    ${PORT_DIR}/src/riscv_vlog_f32.c
+    ${PORT_DIR}/src/absmax_f32.c
+    ${PORT_DIR}/src/add_f32.c
+    ${PORT_DIR}/src/cfft_f32.c
+    ${PORT_DIR}/src/cfft_init_f32.c
+    ${PORT_DIR}/src/cmplx_conj_f32.c
+    ${PORT_DIR}/src/cmplx_dot_prod_f32.c
+    ${PORT_DIR}/src/cmplx_mag_f32.c
+    ${PORT_DIR}/src/cmplx_mult_cmplx_f32.c
+    ${PORT_DIR}/src/dot_prod_f32.c
+    ${PORT_DIR}/src/f32_to_int16.c
+    ${PORT_DIR}/src/int16_to_f32.c
+    ${PORT_DIR}/src/mat_vec_mult_f32.c
+    ${PORT_DIR}/src/multiply_f32.c
+    ${PORT_DIR}/src/nn_classify.c
+    ${PORT_DIR}/src/nn_init.c
+    ${PORT_DIR}/src/offset_f32.c
+    ${PORT_DIR}/src/rfft_f32.c
+    ${PORT_DIR}/src/rfft_init_f32.c
+    ${PORT_DIR}/src/subtract_f32.c
+    ${PORT_DIR}/src/vlog_f32.c
 )
