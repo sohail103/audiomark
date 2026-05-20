7607da3337b15d90e5b254e145236c208f30e7b0
sohail103 <sohailraj.satapathy@gmail.com>
2026-05-20 22:12:35 +0530

    port/riscv: cleanup unused support functions

--- ports/riscv/src/nn/support_functions.h
+++ ports/riscv/src/nn/support_functions.h
@@ -98,8 +98,6 @@ q7_t *nn_mat_mult_kernel_s8_s16(const q7_t          *input_a,
 /* Macros for shortening quantization functions' names and avoid long lines */
 #define MUL_SAT(a, b)  nn_doubling_high_mult((a), (b))
 #define DIV_POW2(a, b) nn_divide_by_power_of_two((a), (b))
-#define EXP_ON_NEG(x)  nn_exp_on_negative_values((x))
-#define ONE_OVER1(x)   nn_one_over_one_plus_x_for_x_in_0_1((x))
 
 static inline int32_t
 nn_doubling_high_mult(const int32_t m1, const int32_t m2)
@@ -176,86 +174,6 @@ nn_requantize(const int32_t val, const int32_t multiplier, const int32_t shift)
         nn_doubling_high_mult_no_sat(val * (1 << LEFT_SHIFT(shift)),
                                      multiplier),
         RIGHT_SHIFT(shift));
-}
-
-/* The following functions are used only for softmax layer, scaled
- * bits = 5 assumed
- */
-
-static inline int32_t
-nn_exp_on_negative_values(int32_t val)
-{
-    int32_t mask  = 0;
-    int32_t shift = 24;
-
-    const int32_t val_mod_minus_quarter
-        = (val & ((1 << shift) - 1)) - (1 << shift);
-    const int32_t remainder = val_mod_minus_quarter - val;
-    const int32_t x
-        = ((int32_t)((uint32_t)val_mod_minus_quarter << 5)) + (1 << 28);
-    const int32_t x2 = MUL_SAT(x, x);
-
-    int32_t result
-        = 1895147668
-          + MUL_SAT(1895147668,
-                    x
-                        + DIV_POW2(MUL_SAT(DIV_POW2(MUL_SAT(x2, x2), 2)
-                                               + MUL_SAT(x2, x),
-                                           715827883)
-                                       + x2,
-                                   1));
-
-#define SELECT_IF_NON_ZERO(x)                                         \
-    {                                                                 \
-        mask   = MASK_IF_NON_ZERO(remainder & (1 << shift++));        \
-        result = SELECT_USING_MASK(mask, MUL_SAT(result, x), result); \
-    }
-
-    SELECT_IF_NON_ZERO(1672461947)
-    SELECT_IF_NON_ZERO(1302514674)
-    SELECT_IF_NON_ZERO(790015084)
-    SELECT_IF_NON_ZERO(290630308)
-    SELECT_IF_NON_ZERO(39332535)
-    SELECT_IF_NON_ZERO(720401)
-    SELECT_IF_NON_ZERO(242)
-
-#undef SELECT_IF_NON_ZERO
-
-    mask = MASK_IF_ZERO(val);
-    return SELECT_USING_MASK(mask, NN_Q31_MAX, result);
-}
-
-static inline int32_t
-nn_mult_by_power_of_two(const int32_t val, const int32_t exp)
-{
-    const int32_t thresh = ((int32_t)(1u << (31 - exp)) - 1);
-
-    if (val > thresh)
-    {
-        return NN_Q31_MAX;
-    }
-    if (val < -thresh)
-    {
-        return NN_Q31_MIN;
-    }
-
-    return (int32_t)(((uint64_t)(uint32_t)val) << exp);
-}
-
-static inline int32_t
-nn_one_over_one_plus_x_for_x_in_0_1(int32_t val)
-{
-    const int64_t sum = (int64_t)val + (int64_t)NN_Q31_MAX;
-    const int32_t half_denominator
-        = (int32_t)((sum + (sum >= 0 ? 1 : -1)) / 2L);
-    int32_t x = 1515870810 + MUL_SAT(half_denominator, -1010580540);
-
-    const int32_t shift = (1 << 29);
-    x += MUL_POW2(MUL_SAT(x, shift - MUL_SAT(half_denominator, x)), 2);
-    x += MUL_POW2(MUL_SAT(x, shift - MUL_SAT(half_denominator, x)), 2);
-    x += MUL_POW2(MUL_SAT(x, shift - MUL_SAT(half_denominator, x)), 2);
-
-    return MUL_POW2(x, 1);
 }
 
 #endif
