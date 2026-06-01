cf38e60f07adaa6e2b3ef9f6951d52a25ddf76d3
JRobinNTA <johnrobin044@gmail.com>
2026-06-01 15:32:16 +0530

    ports/p: Add the macros back to avgpool and change packed init method

--- ports/riscv/p/src/nn/avgpool_25x5x64_s8.c
+++ ports/riscv/p/src/nn/avgpool_25x5x64_s8.c
@@ -25,49 +25,56 @@
  * input: 25x5x64
  * output: 1x1x64
  */
+#define INPUT_Y  25
+#define INPUT_X  5
+#define CHANNELS 64
+#define COUNT    (INPUT_Y * INPUT_X)
+#define HALF     (COUNT / 2)
+/* 1 * 2^21 /125 = 16777 */
+#define MAGIC_COUNT ((1 * 2097152) / COUNT)
+
 int32_t
 nn_avgpool_25x5x64_s8(const q7_t *input_data, q7_t *output_data)
 {
-    /* 1/125 = 0.008.  0.008 * 2^21 = 16777 */
-    int16x4_t V_rec = __riscv_pmv_s_i16x4(16777);
+    int16x4_t V_rec = __riscv_pmv_s_i16x4(MAGIC_COUNT);
 
     /* Process 4 groups (16 channels) at a time. */
-    for (uint8_t c_block = 0; c_block < 4; c_block++)
+    for (uint8_t c_block = 0; c_block < CHANNELS / 16; c_block++)
     {
-        int16x4_t s0 = __riscv_pmv_s_i16x4(0);
-        int16x4_t s1 = __riscv_pmv_s_i16x4(0);
-        int16x4_t s2 = __riscv_pmv_s_i16x4(0);
-        int16x4_t s3 = __riscv_pmv_s_i16x4(0);
+        int16x4_t s0 = { 0 };
+        int16x4_t s1 = { 0 };
+        int16x4_t s2 = { 0 };
+        int16x4_t s3 = { 0 };
 
         /* Start pointer at the current 16-channel block offset */
         const int8_t *in_ptr
             = __builtin_assume_aligned(input_data + (c_block * 16), 4);
 
         /* Walk vertically through the 124 spatial pixels */
-        for (uint8_t spatial = 62; spatial > 0; spatial--)
+        for (uint8_t spatial = HALF; spatial > 0; spatial--)
         {
             int8x4_t v00 = __riscv_pload_i8x4(in_ptr + 0);
-            int8x4_t v01 = __riscv_pload_i8x4(in_ptr + 64 + 0);
+            int8x4_t v01 = __riscv_pload_i8x4(in_ptr + CHANNELS + 0);
             s0           = __riscv_pwadda_i16x4(s0, v00, v01);
 
             int8x4_t v10 = __riscv_pload_i8x4(in_ptr + 4);
-            int8x4_t v11 = __riscv_pload_i8x4(in_ptr + 64 + 4);
+            int8x4_t v11 = __riscv_pload_i8x4(in_ptr + CHANNELS + 4);
             s1           = __riscv_pwadda_i16x4(s1, v10, v11);
 
             int8x4_t v20 = __riscv_pload_i8x4(in_ptr + 8);
-            int8x4_t v21 = __riscv_pload_i8x4(in_ptr + 64 + 8);
+            int8x4_t v21 = __riscv_pload_i8x4(in_ptr + CHANNELS + 8);
             s2           = __riscv_pwadda_i16x4(s2, v20, v21);
 
             int8x4_t v30 = __riscv_pload_i8x4(in_ptr + 12);
-            int8x4_t v31 = __riscv_pload_i8x4(in_ptr + 64 + 12);
+            int8x4_t v31 = __riscv_pload_i8x4(in_ptr + CHANNELS + 12);
             s3           = __riscv_pwadda_i16x4(s3, v30, v31);
 
-            in_ptr += 128;
+            in_ptr += (CHANNELS * 2);
         }
 
         /* Handle the tail */
         const int8_t *tail_ptr = __builtin_assume_aligned(
-            input_data + (c_block * 16) + (124 * 64), 4);
+            input_data + (c_block * 16) + ((COUNT - 1) * CHANNELS), 4);
 
         int8x4_t  vt0  = __riscv_pload_i8x4(tail_ptr + 0);
         int16x4_t vwt0 = __riscv_pwcvt_i16x4(vt0);
